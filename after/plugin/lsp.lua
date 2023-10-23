6f816f56f2b5ed3d1fa0aa9e9c9ac319ee907d3a
Michael Allen <mtallen2014@gmail.com>
2023-10-23 00:21:18 -0500

    Added <leader>ca command to default lsp commands

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,6 +7,7 @@ lsp_zero.on_attach(function(_, bufnr)
     -- to learn the available actions
     lsp_zero.default_keymaps({ buffer = bufnr })
 
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
     -- Create a command `:Format` local to the LSP buffer
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         vim.lsp.buf.format()
@@ -23,16 +24,16 @@ require('mason').setup()
 --  define the property 'filetypes' to the map in question.
 local servers = {
     -- NOTE: Haskell LSP is configured by haskell-tools
-	-- rust_analyzer = {},
-	-- tsserver = {},
-	-- html = { filetypes = { 'html', 'twig', 'hbs'} },
-	lua_ls = {
-		Lua = {
-			workspace = { checkThirdParty = false, library = { vim.env.VIMRUNTIME } },
+    -- rust_analyzer = {},
+    -- tsserver = {},
+    -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+    lua_ls = {
+        Lua = {
+            workspace = { checkThirdParty = false, library = { vim.env.VIMRUNTIME } },
             runtime = { version = 'LuaJIT' },
-			-- telemetry = { enable = false },
-		},
-	},
+            -- telemetry = { enable = false },
+        },
+    },
 }
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
@@ -43,21 +44,16 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local mason_lspconfig = require('mason-lspconfig')
 
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
+    ensure_installed = vim.tbl_keys(servers),
 }
 
 mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end
+    function(server_name)
+        require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+            filetypes = (servers[server_name] or {}).filetypes,
+        }
+    end
 }
-
--- Pass capabilities to haskell_tools
-
-
-
