83505ede0f90c455b76eda92c67cfc5d319e5222
Michael Allen <mtallen2014@gmail.com>
2023-10-27 15:22:37 -0500

    "Added a single command to git add ./, then commit and push"

---
+++ lua/mallen/commands.lua
@@ -0,0 +1,7 @@
+_G.gitCommitAndPush = function(message)
+    local cmd = string.format("git add . && git commit -m '%s' && git push", message)
+    vim.cmd('! ' .. cmd)
+end
+
+vim.api.nvim_command(
+'command -nargs=1 GCP call luaeval("gitCommitAndPush(_A)", <q-args>) " [g]it add ./ && git [c]ommit -m <provided message> && git [p]ush')
--- lua/mallen/init.lua
+++ lua/mallen/init.lua
@@ -1,3 +1,4 @@
 require('mallen.set')
 require('mallen.remap')
+require('mallen.commands')
 require('mallen.packer')
