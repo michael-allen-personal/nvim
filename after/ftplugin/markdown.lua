c75490d28c85c0acb8c9326a2122ca505011df33
Michael Allen <mtallen2014@gmail.com>
2023-11-01 02:08:26 -0500

    Fixed options not being set locally from ftplugin and added desc to the telescope keymaps

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,2 +1,2 @@
-vim.opt.wrap = true
-vim.opt.spell = true
+vim.opt_local.wrap = true
+vim.opt_local.spell = true
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,6 @@
-local builtin = require('telescope.builtin')
--- ff = find files
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- gf = git files
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
--- ps = project search
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = '[f]ind [f]iles' })
+vim.keymap.set('n', '<leader>gf', telescope_builtin.git_files, { desc = 'find [g]it [f]iles' })
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+    telescope_builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end, { desc = '[p]roject [s]earch, (Grep in project base dir)' })
