3c36601cb5706ef14b7dc120f87502333788fc07
Michael Allen <mtallen2014@gmail.com>
2023-11-10 11:36:04 -0600

    Added abbreviations

---
+++ lua/mallen/abbreviations.lua
@@ -0,0 +1,12 @@
+-- Common misspelling fix abbreviations
+vim.cmd('abb teh the')
+vim.cmd('abb adn and')
+vim.cmd('abb tihs this')
+
+-- Regular shorthand abbreviations
+-- something like b/c -> because would go here but vim doesnt support / in
+-- abbreviations because it is a protected charecter
+
+-- Technical shorthand abbreviations
+vim.cmd('abb FP functional programming')
+vim.cmd('abb oauth OAuth')
