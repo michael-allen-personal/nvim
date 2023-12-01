3e1760b4b67172729f37ee2657d848f115190f46
Michael Allen <mtallen2014@gmail.com>
2023-12-01 16:56:22 -0600

    Added some abbreviations, added word to spelling

--- lua/mallen/abbreviations.lua
+++ lua/mallen/abbreviations.lua
@@ -3,10 +3,20 @@ vim.cmd('iab teh the')
 vim.cmd('iab adn and')
 vim.cmd('iab tihs this')
 
+-- Contraction fixes
+vim.cmd("iab cant can't")
+vim.cmd("iab dont don't")
+vim.cmd("iab wont won't")
+vim.cmd("iab yall y'all")
+
+-- Capitalization fixes
+vim.cmd('iab haskell Haskell')
+vim.cmd('iab oauth OAuth')
+vim.cmd('iab oidc OIDC')
+
 -- Regular shorthand abbreviations
 -- something like b/c -> because would go here but vim doesnt support / in
 -- abbreviations because it is a protected charecter
 
 -- Technical shorthand abbreviations
 vim.cmd('iab FP functional programming')
-vim.cmd('iab oauth OAuth')
